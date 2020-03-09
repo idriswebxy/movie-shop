e95e48a390fcdbc3bf00962c51e5ea278556623c
Idris <icoder112@gmail.com>
2020-03-09 00:25:29 -0400

    updates

--- client/src/components/Movies/MovieList.js
+++ client/src/components/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
-import { API_KEY } from "../../config.js";
+// import { API_KEY } from "../../config.js";
 import PropTypes from "prop-types";
 import SpinnerPage from "../Layout/SpinnerPage";
 import { addToCart, loadCart, getCart } from "../../actions/cart";
@@ -32,7 +32,7 @@ const MovieList = ({
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
       .then(res => res.json())
       .then(data => {
